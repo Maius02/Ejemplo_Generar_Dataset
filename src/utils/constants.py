c6028e3a77543403283dd3f7e091e2f8c83c3346
maius02 <maiapaez02@gmail.com>
2025-04-19 19:08:41 -0300

    varios

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 PROJECT_PATH = Path(__file__).parent.parent.parent # Path to the proyect root directory
 
-DATA_PATH = PROJECT_PATH / "data_eph" # Path to the "data_eph" directory
+DATA_PATH = PROJECT_PATH / "files" /"data_eph" # Path to the "data_eph" directory
 
-DATA_OUT_PATH = PROJECT_PATH / "data_out" # Path to the "data_out" directory
+DATA_OUT_PATH = PROJECT_PATH / "files" /"data_out" # Path to the "data_out" directory
 
