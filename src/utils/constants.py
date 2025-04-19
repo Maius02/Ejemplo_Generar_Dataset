abdc3820cee85c03907e3f4a6df9c89b38027793
maius02 <maiapaez02@gmail.com>
2025-04-18 23:22:20 -0300

    desarrolle la funcion que genera dataset

---
+++ src/utils/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_PATH = Path(__file__).parent.parent.parent # Path to the proyect root directory
+
+DATA_PATH = PROJECT_PATH / "data_eph" # Path to the "data_eph" directory
+
+DATA_OUT_PATH = PROJECT_PATH / "data_out" # Path to the "data_out" directory
+
